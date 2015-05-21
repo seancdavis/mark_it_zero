20dd59ba9d80af910233621bc8ffcea5d3fe8483
Sean C Davis <scdavis41@gmail.com>
2015-05-21 06:27:00 -0400

    move assets to more semantic location

---
+++ app/assets/javascripts/mark_it_zero.js
@@ -0,0 +1,4 @@
+//= require jquery
+//= require mark_it_zero/editor
+//= require mark_it_zero/marked
+//= require mark_it_zero/mark_it_zero
--- app/assets/javascripts/mark_it_zero/mark_it_zero.js
+++ app/assets/javascripts/mark_it_zero/mark_it_zero.js
@@ -1,7 +1,3 @@
-//= require jquery
-//= require mark_it_zero/editor
-//= require mark_it_zero/marked
-
 $(document).ready(function(){
   $('textarea.mark-it-zero').each(function(idx){
     new Editor({
